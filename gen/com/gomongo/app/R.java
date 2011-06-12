4b8067f273fe67bd8301407563df6fff482232d7
Dennis <cornwe19@msu.edu>
2011-06-12 12:17:47 -0400

    adding a temporary trashcan icon and switching layout params off from deprecated 'fill_parent' to 'match_parent'

--- gen/com/gomongo/app/R.java
+++ gen/com/gomongo/app/R.java
@@ -19,6 +19,7 @@ public final class R {
         public static final int hat=0x7f020001;
         public static final int icon=0x7f020002;
         public static final int maps_marker=0x7f020003;
+        public static final int trash=0x7f020004;
     }
     public static final class id {
         public static final int button_about=0x7f060009;
