0583f32db3d4c3fc86d92fc7076c3d29e815e02f
Dennis <cornwe19@msu.edu>
2011-06-29 01:21:18 -0400

    Hooking up the get directions button to launch a maps query for directions to the current location

--- src/com/gomongo/app/LocationDetails.java
+++ src/com/gomongo/app/LocationDetails.java
@@ -1,11 +1,16 @@
 package com.gomongo.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.util.Linkify;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class LocationDetails extends Activity {
+public class LocationDetails extends Activity implements OnClickListener {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,5 +32,27 @@ public class LocationDetails extends Activity {
 		phone.setText(intentExtras.getString(MongoLocation.FIELD_PHONE));
 		
 		Linkify.addLinks(phone, Linkify.PHONE_NUMBERS);
+		
+		Button getDirectionsButton = (Button)findViewById(R.id.button_get_directions);
+		getDirectionsButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch( v.getId() ) {
+		case R.id.button_get_directions:
+			String directionsUrl = prepareMapsQueryForThisLocation();
+			Intent directionsIntent = new Intent( Intent.ACTION_VIEW, Uri.parse(directionsUrl) );
+			startActivity(directionsIntent);
+			
+			break;
+		}
+	}
+
+	private String prepareMapsQueryForThisLocation() {
+		String directionsUrlFormat = "http://maps.google.com/maps?daddr=%s";
+		String lineBrokenAddress = getIntent().getExtras().getString(MongoLocation.FIELD_ADDRESS);
+		String directionsUrl = String.format(directionsUrlFormat, lineBrokenAddress.replace("\n", ""));
+		return directionsUrl;
 	}
 }
